85ce7385621b0d7063cda6783b2307e8e92cc0c8
Daniel Jeffery <daniel.jeffery@okta.com>
2023-08-02 14:14:12 -0700

    fix(ci): Using correct activate for test suite

--- client/src/test/extension.test.ts
+++ client/src/test/extension.test.ts
@@ -1,44 +1,19 @@
 
-import { join } from 'path';
+
 import * as fs from 'fs';
 import assert = require('assert');
-import { activate } from '../extension';
-import { ExtensionContext, TextDocument, commands, window, workspace } from 'vscode';
-import { getDocUri } from './helper';
+import { TextDocument, commands, window } from 'vscode';
+import { getDocUri, activate } from './helper';
 import { friendlySyntaxToApiSyntax } from '@openfga/syntax-transformer';
 
 
 suite('Should execute command', () => {
 
+	const docUri = getDocUri('test.fga');
+
 	test('Generates expected JSON', async () => {
-		const context: ExtensionContext = {
-			subscriptions: [],
-			workspaceState: undefined,
-			globalState: undefined,
-			secrets: undefined,
-			extensionUri: undefined,
-			extensionPath: '',
-			environmentVariableCollection: undefined,
-			asAbsolutePath: function (relativePath: string): string {
-				throw new Error('Function not implemented.');
-			},
-			storageUri: undefined,
-			storagePath: '',
-			globalStorageUri: undefined,
-			globalStoragePath: '',
-			logUri: undefined,
-			logPath: '',
-			extensionMode: undefined,
-			extension: undefined
-		};
 
-		// Attempt to activate context
-		activate(context);
-
-		// Assert command assigned assigned for disposal
-		assert.equal(context.subscriptions.length, 1);
-
-		const docUri = getDocUri('test.fga');
+		await activate(docUri);
 
 		// Get editor window
 		const editor = await window.showTextDocument(docUri);
