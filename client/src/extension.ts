c1f3de257bb6632ffad45f6d7eb1d4984f66bb07
Daniel Jeffery <daniel.jeffery@okta.com>
2023-08-03 11:29:26 -0700

    feat: Recoupling language server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -7,7 +7,52 @@ import * as path from 'path';
 import { window, workspace, ExtensionContext, commands, Range } from 'vscode';
 import { friendlySyntaxToApiSyntax } from '@openfga/syntax-transformer';
 
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
 export function activate(context: ExtensionContext) {
+
+	// The server is implemented in node
+	const serverModule = context.asAbsolutePath(
+		path.join('server', 'out', 'server.js')
+	);
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+		}
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'openfga' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'openfgaLanguageServer',
+		'OpenFGA Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
 
 	const transformCommand = commands.registerCommand('openfga.commands.transformToJson', async () => {
 		const activeEditor = window.activeTextEditor;
@@ -27,4 +72,11 @@ export function activate(context: ExtensionContext) {
 	});
 
 	context.subscriptions.push(transformCommand);
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
