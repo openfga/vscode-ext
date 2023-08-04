ebd4b72ee8cea87a79a14e8db49c57f7f53c1ed8
Daniel Jeffery <1425457+d-jeffery@users.noreply.github.com>
2023-08-04 08:01:43 -0700

    feat: Language server with validation (#48)
    Co-authored-by: Daniel Jeffery <daniel.jeffery@okta.com>

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,13 +1,45 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import * as path from 'path';
 import { window, workspace, ExtensionContext, commands, Range } from 'vscode';
 import { friendlySyntaxToApiSyntax } from '@openfga/syntax-transformer';
 
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
 export function activate(context: ExtensionContext) {
+
+	const module = path.join(__dirname, '..', '..', 'server', 'dist', 'server.js');
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6012'] };
+	const serverOptions: ServerOptions = {
+		run: { module, transport: TransportKind.ipc },
+		debug: { module, transport: TransportKind.ipc, options: debugOptions}
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'openfga' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'openfgaLanguageServer',
+		'OpenFGA Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
 
 	const transformCommand = commands.registerCommand('openfga.commands.transformToJson', async () => {
 		const activeEditor = window.activeTextEditor;
@@ -27,4 +59,11 @@ export function activate(context: ExtensionContext) {
 	});
 
 	context.subscriptions.push(transformCommand);
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
---
+++ server/src/server.ts
@@ -0,0 +1,142 @@
+
+import {
+	createConnection,
+	TextDocuments,
+	Diagnostic,
+	DiagnosticSeverity,
+	ProposedFeatures,
+	InitializeParams,
+	DidChangeConfigurationNotification,
+	CompletionItem,
+	CompletionItemKind,
+	TextDocumentPositionParams,
+	TextDocumentSyncKind,
+	InitializeResult
+} from 'vscode-languageserver/node';
+
+import {
+	TextDocument
+} from 'vscode-languageserver-textdocument';
+
+import { checkDSL } from '@openfga/syntax-transformer';
+
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+	const capabilities = params.capabilities;
+
+	// Does the client support the `workspace/configuration` request?
+	// If not, we fall back using global settings.
+	hasConfigurationCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.configuration
+	);
+	hasWorkspaceFolderCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.workspaceFolders
+	);
+	hasDiagnosticRelatedInformationCapability = !!(
+		capabilities.textDocument &&
+		capabilities.textDocument.publishDiagnostics &&
+		capabilities.textDocument.publishDiagnostics.relatedInformation
+	);
+
+	const result: InitializeResult = {
+		capabilities: {
+			textDocumentSync: TextDocumentSyncKind.Incremental,
+			// Tell the client that this server supports code completion.
+			completionProvider: {
+				resolveProvider: false
+			}
+		}
+	};
+	if (hasWorkspaceFolderCapability) {
+		result.capabilities.workspace = {
+			workspaceFolders: {
+				supported: true
+			}
+		};
+	}
+	return result;
+});
+
+connection.onInitialized(() => {
+	if (hasConfigurationCapability) {
+		// Register for all configuration changes.
+		connection.client.register(DidChangeConfigurationNotification.type, undefined);
+	}
+	if (hasWorkspaceFolderCapability) {
+		connection.workspace.onDidChangeWorkspaceFolders(_event => {
+			connection.console.log('Workspace folder change event received.');
+		});
+	}
+});
+
+
+connection.onDidChangeConfiguration(change => {
+	// Revalidate all open text documents
+	documents.all().forEach(validateTextDocument);
+});
+
+
+// Only keep settings for open documents
+// documents.onDidClose(e => {
+// 	documentSettings.delete(e.document.uri);
+// });
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+	validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+
+	const result = checkDSL(textDocument.getText());
+
+	const diagnostics: Diagnostic[] = result.map((e: any)  => {
+		const diagnostic: Diagnostic = {
+			severity: DiagnosticSeverity.Error,
+			range: {
+				start: {line: Math.max(e.startLineNumber - 1, 0), character: Math.max(e.startColumn - 1, 0)},
+				end: {line: Math.max(e.endLineNumber - 1, 0), character: Math.max(e.endColumn - 1, 0)}
+			},
+			message: e.message,
+			source: e.source
+		};
+		return diagnostic;
+	});
+
+	// Send the computed diagnostics to VSCode.
+	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+connection.onDidChangeWatchedFiles(_change => {
+	// Monitored files have change in VSCode
+	connection.console.log('We received an file change event');
+});
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+		// The pass parameter contains the position of the text document in
+		// which code complete got requested. For the example we ignore this
+		// info and always provide the same completion items.
+		return [];
+	}
+);
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
