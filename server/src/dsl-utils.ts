e9c3cb6ad0ecc016c690f6db6919f75a843f6a84
Daniel Jeffery <1425457+d-jeffery@users.noreply.github.com>
2024-02-23 17:59:04 -0800

    chore: version bump to syntax-transformer (#176)

--- server/src/dsl-utils.ts
+++ server/src/dsl-utils.ts
@@ -17,36 +17,43 @@ export function getDiagnosticsForDsl(dsl: string): Diagnostic[] {
 export const createDiagnostics = (err: errors.DSLSyntaxError | errors.ModelValidationError): Diagnostic[] => {
   return (
     err.errors.map((e: errors.DSLSyntaxSingleError | errors.ModelValidationSingleError) => {
-      const lines = e.getLine(-1);
-      let characters;
-      let source;
+      const linesStart = (e.line?.start || 1) - 1;
+      const lineEnd = (e.line?.end || 1) - 1;
+      const charactersStart = e.column?.start || 1;
+      const charactersEnd = e.column?.end || 1;
 
       let code = errors.ValidationError.InvalidSyntax;
 
       if (e instanceof errors.DSLSyntaxSingleError) {
-        characters = e.getColumn();
-        source = "SyntaxError";
+        return {
+          message: e.msg,
+          severity: DiagnosticSeverity.Error,
+          range: {
+            start: { line: linesStart, character: charactersStart },
+            end: { line: lineEnd, character: charactersEnd },
+          },
+          source: "SyntaxError",
+          code,
+          data: e.metadata,
+        };
       } else if (e instanceof errors.ModelValidationSingleError) {
-        characters = e.getColumn(-1);
-        source = "ModelValidationError";
         if (e.metadata?.errorType) {
           code = e.metadata.errorType;
         }
+        return {
+          message: e.msg,
+          severity: DiagnosticSeverity.Error,
+          range: {
+            start: { line: linesStart, character: charactersStart - 1 },
+            end: { line: lineEnd, character: charactersEnd - 1 },
+          },
+          source: "ModelValidationError",
+          code,
+          data: e.metadata,
+        };
       } else {
         throw new Error("Unhandled Exception: " + JSON.stringify(e, null, 4));
       }
-
-      return {
-        message: e.msg,
-        severity: DiagnosticSeverity.Error,
-        range: {
-          start: { line: lines.start, character: characters.start },
-          end: { line: lines.end, character: characters.end },
-        },
-        source,
-        code,
-        data: e.metadata,
-      };
     }) || []
   );
 };
