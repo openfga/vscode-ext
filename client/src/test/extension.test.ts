88b365389d1c7cbb03a6f4871a48926ddaaf7d79
Daniel Jeffery <danielajeffery@gmail.com>
2024-03-28 12:16:42 -0700

    feat: Modular Model from DSL to JSON

--- client/src/test/extension.test.ts
+++ client/src/test/extension.test.ts
@@ -5,9 +5,9 @@ import { getDocUri, activate } from "./helper";
 import { transformer } from "@openfga/syntax-transformer";
 
 suite("Should execute command", () => {
-  const docUri = getDocUri("test.fga");
-
   test("Generates expected JSON", async () => {
+    const docUri = getDocUri("test.fga");
+
     await activate(docUri);
 
     // Get editor window
@@ -25,5 +25,28 @@ suite("Should execute command", () => {
     // Ensure result from command is the same as result from method.
     assert.equal(editor.document.getText(), original);
     assert.equal(resultFromCommand.getText(), resultFromMethodCall);
+  });
+
+  test("Generates expected JSON from modular model", async () => {
+    const docUri = getDocUri("modular-models/model/core.fga");
+
+    await activate(docUri);
+
+    // Get editor window
+    await window.showTextDocument(docUri);
+
+    // Get result from running command against what is in editor window
+    const resultFromCommand = JSON.parse(
+      (await commands.executeCommand<TextDocument>("openfga.commands.transformToJson")).getText(),
+    );
+
+    // Get original document
+    const original = JSON.parse(
+      String.fromCharCode.apply(null, await workspace.fs.readFile(getDocUri("modular-models/output.json"))),
+    );
+
+    // Call transform directly for comparison, using original doc
+    assert.equal(JSON.stringify(resultFromCommand), JSON.stringify(original));
+    assert.equal(transformer.transformJSONToDSL(resultFromCommand), transformer.transformJSONToDSL(original));
   });
 });
---
+++ client/testFixture/modular-models/fga.mod
@@ -0,0 +1,6 @@
+schema: '1.2'
+contents:
+  - model/core.fga
+  - model/jira/projects.fga
+  - model/jira/tickets.fga
+  - model/confluence.fga
