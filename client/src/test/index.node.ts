4bf6d9895e78500b919f392355fe41e82802f64d
Raghd Hamzeh <raghd.hamzeh@openfga.dev>
2024-09-06 11:17:34 -0400

    chore: fix test runner

--- client/src/test/index.node.ts
+++ client/src/test/index.node.ts
@@ -16,28 +16,25 @@ export function run(): Promise<void> {
 
   const testsRoot = __dirname;
 
-  return new Promise((resolve, reject) => {
-    glob("**.test.js", { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return reject(err);
-      }
+  return glob.glob("**.test.js", { cwd: testsRoot }).then(
+    (files) =>
+      new Promise((resolve, reject) => {
+        // Add files to the test suite
+        files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            reject(new Error(`${failures} tests failed.`));
-          } else {
-            resolve();
-          }
-        });
-      } catch (err) {
-        console.error(err);
-        reject(err);
-      }
-    });
-  });
+        try {
+          // Run the mocha test
+          mocha.run((failures) => {
+            if (failures > 0) {
+              reject(new Error(`${failures} tests failed.`));
+            } else {
+              resolve();
+            }
+          });
+        } catch (err) {
+          console.error(err);
+          reject(err);
+        }
+      }),
+  );
 }
